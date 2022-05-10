e3a5041bfeabb1a2815780d3b4e8e678e699996a
Paul Grin <pashkagrin@gmail.com>
2022-05-10 16:33:25 +0300

    layout created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {useState, useEffect} from "react";
+import Navbar from './Components/Navbar/Navbar'
+import MainPage from "./Components/MainPage/MainPage";
+import InitialPage from "./Components/InitialPage/InitialPage";
+import UserNotFound from "./Components/UserNotFound/UserNotFound";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <Navbar/>
+        <MainPage/>
     </div>
   );
 }
